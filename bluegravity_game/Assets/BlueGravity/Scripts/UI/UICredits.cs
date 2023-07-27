1dc3ce8ba28cb9b2015a321bae7a61ad45a6852c
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-07-27 20:11:45 -0300

    Configured credits scene

---
+++ bluegravity_game/Assets/BlueGravity/Scripts/UI/UICredits.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using BlueGravity.Managers;
+
+namespace BlueGravity.UI
+{
+    public class UICredits : MonoBehaviour
+    {
+        [Header("Scenes")]
+        [SerializeField] private string mainMenuSceneName = "";
+
+        public void LoadMainMenuScene()
+        {
+            LoaderManager.Get().LoadScene(mainMenuSceneName);
+        }
+
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+    }
+}
