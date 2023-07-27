cd35271162e5e0ec8cce4bd7a9aa5f8d30f4173d
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-07-27 19:53:39 -0300

    Configured main menu scene

---
+++ bluegravity_game/Assets/BlueGravity/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using BlueGravity.Managers;
+
+namespace BlueGravity.UI
+{
+    public class UIMainMenu : MonoBehaviour
+    {
+        [Header("Scenes")]
+        [SerializeField] private string gameSceneName = "";
+        [SerializeField] private string creditsSceneName = "";
+
+        public void LoadGameScene()
+        {
+            LoaderManager.Get().LoadScene(gameSceneName);
+        }
+
+        public void LoadCreditsScene()
+        {
+            LoaderManager.Get().LoadScene(creditsSceneName);
+        }
+
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+    }
+}
